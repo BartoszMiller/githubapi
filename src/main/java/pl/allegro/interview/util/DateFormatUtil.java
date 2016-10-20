774170af224a4062619fea70a2dd059f47d517c3
Bartosz Miller <miller.bartek@gmail.com>
2016-10-20 18:16:17 +0100

    Deleted unused package and class

--- src/main/java/pl/allegro/interview/util/DateFormatUtil.java
+++
@@ -1,9 +0,0 @@
-package pl.allegro.interview.util;
-
-public class DateFormatUtil {
-
-    private DateFormatUtil() {
-    }
-
-    public static final String ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-}
